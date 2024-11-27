44260b93383332e3cb78bf141337249dc1e9c346
Wheza <wheza99@gmail.com>
2024-11-27 13:13:12 +0700

    Center Column
    Menambahkan component untuk center column pada aplikasi.

--- 1.1 Landing Page/app/_components/_index.js
+++ 1.1 Landing Page/app/_components/_index.js
@@ -1,6 +1,13 @@
 import ResponsiveContainer from "./responsive_container";
 import TwoColumnLayout from "./two_column_layout";
+import CenterColumn from "./center_column";
 import NavigationBar from "./navigation_bar";
 import Footer from "./footer";
 
-export { ResponsiveContainer, TwoColumnLayout, NavigationBar, Footer };
+export {
+  ResponsiveContainer,
+  TwoColumnLayout,
+  CenterColumn,
+  NavigationBar,
+  Footer,
+};
