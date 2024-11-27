99c6fa06f5271cd5058a7fe1f3ea8b6f89fa7f66
Wheza <wheza99@gmail.com>
2024-11-27 13:24:51 +0700

    Implement Hero Section
    Implementasi Hero Section ke dalam Landing Page.

---
+++ 1.1 Landing Page/app/home/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeroSection } from "./_components/_index";
+
+function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
+
+export default HomePage;
