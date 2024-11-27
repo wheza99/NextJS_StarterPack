9246c611c101ec50e64796bb27dd09397928c3b1
Wheza <wheza99@gmail.com>
2024-11-27 09:01:46 +0700

    Navigation Bar
    Membuat navigation bar component.

--- 1.1 Landing Page/app/_components/_index.js
+++ 1.1 Landing Page/app/_components/_index.js
@@ -1,4 +1,5 @@
 import ResponsiveContainer from "./responsive_container";
 import TwoColumnLayout from "./two_column_layout";
+import NavigationBar from "./navigation_bar";
 
-export { ResponsiveContainer, TwoColumnLayout };
+export { ResponsiveContainer, TwoColumnLayout, NavigationBar };
